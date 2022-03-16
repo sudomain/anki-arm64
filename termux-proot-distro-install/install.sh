61f312dbafcb1505fef11c170be8dca9f14d1b02
sudomain <sudomain@github>
2022-03-16 12:08:09 -0400

    cleaned up install script, added instructions to README

--- termux-proot-distro-install/install.sh
+++ termux-proot-distro-install/install.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 # Commands needed to run once to install Anki in a proot-distro OS. Keeps the run script faster
 
-# Variables file used to configure behavior of the install and run scripts
-. ./variables.sh
+# $1 - logs in as a particular user in the proot (note root is the root user in the proot, not android)
+# $@ - command(s) to run as that user
+run_command_in_proot (){
+    COMMAND_USER="$1"
+    shift 1
+    # if you run into issues, try removing --isolated
+    proot-distro login --isolated --user "$COMMAND_USER" "$DISTRO" -- "$@"
+}
 
-# fyi Termux's pacakge manager "pkg" is a wrapper around apt.
-# apt does have an option to automatically upgrade packages (--assume-yes) but I don't think this should be used.
+# Package(s) needed in vanilla Termux
+TERMUX_PACKAGES="proot-distro"
+
+# proot-distro distribution to install. At the time of this writing, every option has a wrong version of libc that anki needs, with the exception of Ubuntu. This could change.
+DISTRO="ubuntu"
+
+# User to install anki under in the proot environment. Probably only useful to existing proot-distro users that have $DISTRO installed
+ANKI_INSTALL_USER="anki-user"
+
+# Path to copy the run-anki.sh script to
+ANKI_USER_BIN="/home/$ANKI_INSTALL_USER/bin/"
+
+# fyi Termux's pacakge manager "pkg" is a wrapper around apt. apt has an option to automatically upgrade packages (--assume-yes) but I don't think this should be used.
 pkg upgrade
 pkg install "$TERMUX_PACKAGES"
 proot-distro install "$DISTRO"
@@ -13,21 +30,25 @@ proot-distro install "$DISTRO"
 
 # Qt (and thus Anki) doesn't like running as an user named root, even if it is a fake root like proot.
 run_command_in_proot root adduser --disabled-password --gecos "" "$ANKI_INSTALL_USER"
+run_command_in_proot "$ANKI_INSTALL_USER" mkdir -p "$ANKI_USER_BIN" &&
+cp ~/anki-arm64/termux-proot-distro-install/run-anki.sh "$PREFIX/var/lib/proot-distro/installed-rootfs/$DISTRO$ANKI_USER_BIN"
 
-# Anki requires an X server at minumum and a somewhat intuitive window manager to be useable. My preference for the X server is tigervnc and to connect to it with a VNC client for Android. For the window manager, xfwm4 fron Xfce will do. Customization/use of lighter-weight software can be done here.
-X_PACKAGES="tigervnc-standalone-server xfce4"
+
+# Dependencies for Anki under Ubuntu (different distros may not come with the same packages Ubuntu does or have packages with slightly different names). Anki requires an X server at minumum and a somewhat intuitive window manager to be useable. My preference for the X server is tigervnc and to connect to it with a VNC client for Android. For the window manager, xfwm4 fron Xfce will do. Customization/use of lighter-weight software can be done here.
+ANKI_PACKAGES="dialog tigervnc-standalone-server xfce4 mpv python3.9-dev python3.9-venv python3-pyqt5.qtwebengine python3-pyqt5.qtmultimedia"
 
 # Anki dependencies
-run_command_in_proot root apt install python3-pyqt5.qtwebengine python3-pyqt5.qtmultimedia $X_PACKAGES
+run_command_in_proot root apt update
+#run_command_in_proot root apt dist-upgrade
+run_command_in_proot root apt install $ANKI_PACKAGES
 
 # Get the latest Anki stable release via pip - https://betas.ankiweb.net/#via-pypipip
 run_command_in_proot "$ANKI_INSTALL_USER" python3.9 -m venv --system-site-packages pyenv
 
-# The following is recommended by  the instructions above, but seems to be broken due to: https://github.com/pypa/pip/issues/10887
+# The following is recommended by  the instructions above, but seems to be broken. Can be uncommented once this is fixed: https://github.com/pypa/pip/issues/10887
 #run_command_in_proot "$ANKI_INSTALL_USER" pyenv/bin/pip install --upgrade pip
 
 # note adding --pre flag after pip install will get the latest beta. without it, get the latest stable 
 run_command_in_proot "$ANKI_INSTALL_USER" pyenv/bin/pip install --upgrade aqt
 
-#TODO copy run.sh to the user's PATH
 #TODO handle existing users upgrading their anki using pip
--- termux-proot-distro-install/run-anki.sh
+++ termux-proot-distro-install/run-anki.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# a script to setup the proot environment for anki.
+# A script to setup the minimum proot environment for Anki. Namely a X11/VNC server to connect to, a window manager, and set variables that Qt / Anki need.
 
 export DISPLAY=":1"
 
@@ -10,9 +10,16 @@ export QTWEBENGINE_CHROMIUM_FLAGS="--disable-gpu"
 # https://forums.ankiweb.net/t/setting-disable-seccomp-filter-sandbox-by-default-on-linux/13765
 export QTWEBENGINE_DISABLE_SANDBOX=1
 
-# Connections are bound to localhost for security, but some interesting configs could be made via ssh port forwarding
-tigervncserver -localhost -xstartup /usr/bin/xfce4-session $DISPLAY
+# ANKI_BASE is a configurable path to the directory containing profiles (User 1, User 2, ...), add-ons (addons21/), and the prefs.db file. Depending on your Android version, you might be able to set this to shared storage to possibly prevent the collection from being deleted if Termux is accidentally uninstalled.
+# export ANKI_BASE="/sdcard/Anki/"
 
+# Width and Height of the VNC desktop to be created, thus the size of windows when maximized (minus panel / window manager decorations). 1080x1920 is a reasonable assumption for phones in portrait mode at the time of this writing, but can be changed to match your display.
+export DESKTOP_GEOMETRY="1080x1920"
+
+# Connections are bound to localhost for security, but secure remote connections could be made using ssh port forwarding.
+tigervncserver -localhost -xstartup /usr/bin/xfce4-session -geometry "$DESKTOP_GEOMETRY" $DISPLAY
+
+# Run the Anki in the installed virtual environment.
 /home/anki-user/pyenv/bin/anki
 
 #TODO `am start` to start a vnc-viewer app would be a nice touch
